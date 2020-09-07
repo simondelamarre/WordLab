0abbc4aa60275d161eeb0d8833aa758b4feddfe2
simondelamarre <landscape.viewer@gmail.com>
2020-09-07 16:20:31 +0200

    clean beta0

--- dist/types/DispatchMode.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DispatchMode = void 0;
-var DispatchMode;
-(function (DispatchMode) {
-    DispatchMode[DispatchMode["Circus"] = 1] = "Circus";
-    DispatchMode[DispatchMode["Linear"] = 2] = "Linear";
-    DispatchMode[DispatchMode["Unbelieveable"] = 3] = "Unbelieveable";
-})(DispatchMode = exports.DispatchMode || (exports.DispatchMode = {}));
